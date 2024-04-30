da7f583246fbef4bbd2c67723c8bca240154e00f
JenngsikEom <105258488+JenngsikEom@users.noreply.github.com>
2024-04-30 17:04:03 +0900

    test

--- src/test/java/com/guide/ex/GuideApplicationTests.java
+++ src/test/java/com/guide/ex/GuideApplicationTests.java
@@ -26,7 +26,7 @@ class GuideApplicationTests {
 
         log.info("-----------------------------------");
         log.info(con.toString());
-        log.info("-----------------------------------");
+        log.info("----------------------------...-------");
         Assertions.assertNotNull(con);
     }
 
