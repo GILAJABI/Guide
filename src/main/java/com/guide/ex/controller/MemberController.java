2f55967a4c9e51e16f57ab73a98ab665ba29d918
CHAERIM LEE <leechaerim214@gmail.com>
2024-05-01 15:38:17 +0900

    feat: 회원 관련 페이지 / f

--- src/main/java/com/guide/ex/controller/MemberController.java
+++ src/main/java/com/guide/ex/controller/MemberController.java
@@ -2,12 +2,17 @@ package com.guide.ex.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
+    @GetMapping("/login")
+    public void login() {
+
+    }
 
 }
