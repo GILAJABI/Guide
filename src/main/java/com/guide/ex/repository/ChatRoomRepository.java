63c6ec87c1d9b1f01afd3187d87ca45b64e1e3f4
JenngsikEom <105258488+JenngsikEom@users.noreply.github.com>
2024-05-02 16:15:50 +0900

    test4

--- src/main/java/com/guide/ex/repository/ChatRoomRepository.java
+++ src/main/java/com/guide/ex/repository/ChatRoomRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     @Query(value = "select now()", nativeQuery = true)
     String getTime();
+
+    ChatRoom createChatRoom(String user1, String user2);
+
+    ChatRoom findRoomById(String roomId);
 }
