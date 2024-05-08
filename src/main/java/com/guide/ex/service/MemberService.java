becdea49e29104943013c2cdaf68ef1b5722b570
Hoje0715 <ho9760@naver.com>
2024-05-08 19:58:42 +0900

    refactor: 코드정리

--- src/main/java/com/guide/ex/service/MemberService.java
+++ src/main/java/com/guide/ex/service/MemberService.java
@@ -12,18 +12,9 @@ public interface MemberService {
     boolean login(String uid, String pwd);
     Long setLoginSession(String uid);
     boolean isIdAlreadyExists(String uid);
-//    void fileUpload(MemberProfileDTO dto);
     void fileUpload(MemberProfileDTO dto, MultipartFile file);
-
-    //    MemberProfileDTO memberInfo(Long member_id);
-//    MemberDTO readOne(Long member_id);
-//    void modify(MemberProfileDTO memberProfileDTO);
-    //아래 함수는 DTO로 ModelMapper를 활용해 수정
-//    MemberProfile memberInfo(Long member_id);
-    //프로필 id 세션 할당 값
     boolean setProfileSession(Long member_id);
     MemberDTO memberReadOne(Long member_id);
-//    void profileModify(MemberDTO memberDTO, MemberProfileDTO memberProfileDTO);
     void profileModify(MemberProfileDTO memberProfileDTO);
     void profileRegister(MemberProfileDTO memberProfileDTO);
     List<MemberDTO> findProfileMember();
