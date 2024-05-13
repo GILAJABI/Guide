9a5a24b4a89ebf09075b177acd153eee9da9ef1c
Hoje0715 <ho9760@naver.com>
2024-05-13 10:00:35 +0900

    fix: merge버그 수정

--- src/main/java/com/guide/ex/controller/post/PostController.java
+++ src/main/java/com/guide/ex/controller/post/PostController.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Log4j2
