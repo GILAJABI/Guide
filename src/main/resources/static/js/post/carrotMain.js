c2f106f4bf8126446774b8a65e9a435c4716183e
CHAERIM LEE <leechaerim214@gmail.com>
2024-05-14 12:31:39 +0900

    fix: 게시판 프론트 수정 / f

--- src/main/resources/static/js/post/carrotMain.js
+++ src/main/resources/static/js/post/carrotMain.js
@@ -1,6 +1,10 @@
-document.querySelectorAll('.filter-link').forEach(link => {
-    link.addEventListener('click', function() {
-        document.querySelectorAll('.filter-link').forEach(lnk => lnk.classList.remove('active'));
-        this.classList.add('active');
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('#sortMenu .filter-link');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // 페이지 리로드 방지
+            links.forEach(node => node.classList.remove('active')); // 모든 링크에서 active 클래스 제거
+            link.classList.add('active'); // 클릭된 링크에 active 클래스 추가
+        });
     });
 });
